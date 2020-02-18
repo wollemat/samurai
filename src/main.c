dbf7b1b09375f9d1230049b471c049b2f8d02591
wollemat <f.c.slothouber@gmail.com>
2020-02-18 01:54:01 +0100

    Implement initial password generation

--- src/main.c
+++ src/main.c
@@ -1,8 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!\n");
+const int true = 1;
+const int false = 0;
+
+const char *vowels = "aeiouy";
+const char *consonants = "bcdfghjklmnpqrstvwxz";
+const char *specials = "!@#$%^&*-+?";
+
+unsigned char *generate_password(int length, int capitalized, int specialized) {
+    unsigned char* pwd = calloc(length + 1, sizeof(char));
+
+    int sign = true;
+    for (int i = 0; i < length; i++) {
+        pwd[i] = (sign) ? consonants[0] : vowels[0];
+        sign = (sign) ? false : true;
+    }
+
+    return pwd;
+}
+
+int main(int argc, char **argv) {
+    unsigned char *pwd = generate_password(12, true, true);
+
+    printf("%s\n", pwd);
+
+    free(pwd);
 
     exit(EXIT_SUCCESS);
 }

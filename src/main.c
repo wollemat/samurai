d220ac2acbeb5cafd9667315f6049ec0e64614de
wollemat <f.c.slothouber@gmail.com>
2020-05-20 18:25:04 +0200

    Implement seed command

--- src/main.c
+++ src/main.c
@@ -38,13 +38,15 @@ int main(int argc, char **argv) {
     int number_amount = 0;
     int password_amount = 1;
     int iterate = 128;
+    int seed = clock();
 
     if (argc > 1 && strcmp(argv[1], "-h") == 0) {
         printf("Samurai: The Human Readable Password Generator\n\n");
-        printf("Usage: samurai [-h] [-x <int>] [-i <int>] [-l <int>] [-c] [-s] [-n]\n\n");
+        printf("Usage: samurai [-h] [-x <int>] [-i <int>] [--seed <int>] [-l <int>] [-c] [-s] [-n]\n\n");
         printf("-h:\t\t Print this help page.\n");
         printf("-x <int>:\t Set the amount of generated passwords. Defaults to %d.\n", password_amount);
         printf("-i <int>:\t Iterate this many times before generating each password. Defaults to %d.\n", iterate);
+        printf("--seed <int>:\t Use this number as seed. Defaults to clock ticks since program start.\n");
         printf("-l <int>:\t Set the length of the generated passwords. Defaults to %d.\n", length);
         printf("-n <int>:\t Set the amount of numbers in the generated passwords. Defaults to %d.\n", number_amount);
         printf("-c:\t\t Enable capitalization of the generated passwords.\n");
@@ -79,13 +81,19 @@ int main(int argc, char **argv) {
                 printf("\"%s\" is not an integer value.\n", argv[i]);
                 exit(EXIT_FAILURE);
             }
+        } else if (strcmp(argv[i], "--seed") == 0) {
+            i++;
+            if (sscanf(argv[i], "%d", &seed) == 0) {
+                printf("\"%s\" is not an integer value.\n", argv[i]);
+                exit(EXIT_FAILURE);
+            }
         } else {
             printf("\"%s\" is not a valid flag. Run `samurai -h` for usage information.\n", argv[i]);
             exit(EXIT_FAILURE);
         }
     }
 
-    srand(clock());
+    srand(seed);
 
     for (int i = 0; i < password_amount; i++) {
         for (int j = 0; j < iterate; j++) {

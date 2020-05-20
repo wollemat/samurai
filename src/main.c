2be2797a516ad99624998fb459e855688b79ee84
wollemat <f.c.slothouber@gmail.com>
2020-05-20 18:10:55 +0200

    Implement iteration before generation

--- src/main.c
+++ src/main.c
@@ -12,10 +12,9 @@ const char *consonants = "bcdfghjklmnpqrstvwxz";
 const char *numbers = "0123456789";
 const char *specials = "!@#$%^&*-+?";
 
-unsigned char *generate_password(int length, int capitalized, int specialized, int number_amount, int seed) {
+unsigned char *generate_password(int length, int capitalized, int specialized, int number_amount) {
     unsigned char *pwd = calloc(length + 1, sizeof(char));
     int sign = TRUE;
-    srand(seed);
 
     for (int i = 0; i < length; i++) {
         pwd[i] = (sign) ? consonants[rand() % strlen(consonants)] : vowels[rand() % strlen(vowels)];
@@ -38,14 +37,16 @@ int main(int argc, char **argv) {
     int specialized = FALSE;
     int number_amount = 0;
     int password_amount = 1;
+    int iterate = 128;
 
     if (argc > 1 && strcmp(argv[1], "-h") == 0) {
-        printf("Samurai: Human Readable Password Generator\n\n");
-        printf("Usage: samurai [-h] [-x <int>] [-l <int>] [-c] [-s] [-n]\n\n");
+        printf("Samurai: The Human Readable Password Generator\n\n");
+        printf("Usage: samurai [-h] [-x <int>] [-i <int>] [-l <int>] [-c] [-s] [-n]\n\n");
         printf("-h:\t\t Print this help page.\n");
-        printf("-x <int>:\t Set the amount of generated passwords. Defaults to 1.\n");
-        printf("-l <int>:\t Set the length of the generated passwords. Defaults to 12.\n");
-        printf("-n <int>:\t Set the amount of numbers in the generated passwords. Defaults to 0.\n");
+        printf("-x <int>:\t Set the amount of generated passwords. Defaults to %d.\n", password_amount);
+        printf("-i <int>:\t Iterate this many times before generating each password. Defaults to %d.\n", iterate);
+        printf("-l <int>:\t Set the length of the generated passwords. Defaults to %d.\n", length);
+        printf("-n <int>:\t Set the amount of numbers in the generated passwords. Defaults to %d.\n", number_amount);
         printf("-c:\t\t Enable capitalization of the generated passwords.\n");
         printf("-s:\t\t Add a special character to the generated passwords.\n");
         exit(EXIT_SUCCESS);
@@ -72,14 +73,26 @@ int main(int argc, char **argv) {
                 printf("\"%s\" is not an integer value.\n", argv[i]);
                 exit(EXIT_FAILURE);
             }
+        } else if (strcmp(argv[i], "-i") == 0) {
+            i++;
+            if (sscanf(argv[i], "%d", &iterate) == 0) {
+                printf("\"%s\" is not an integer value.\n", argv[i]);
+                exit(EXIT_FAILURE);
+            }
         } else {
             printf("\"%s\" is not a valid flag. Run `samurai -h` for usage information.\n", argv[i]);
             exit(EXIT_FAILURE);
         }
     }
 
+    srand(clock());
+
     for (int i = 0; i < password_amount; i++) {
-        unsigned char *pwd = generate_password(length, capitalized, specialized, number_amount, clock());
+        for (int j = 0; j < iterate; j++) {
+            rand();
+        }
+
+        unsigned char *pwd = generate_password(length, capitalized, specialized, number_amount);
         printf("%s\n", pwd);
         free(pwd);
     }

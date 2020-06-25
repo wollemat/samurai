b8808bc2a170f41f59f15e37cad35cf0ccad4bd8
wollemat <f.c.slothouber@gmail.com>
2020-06-25 19:10:18 +0200

    Remove redundant commands and flags

--- src/main.c
+++ src/main.c
@@ -3,27 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *version = "V1.2.0";
+const char *version = "2.0.0"; // Current version number of Samurai.
 
-const char *flag_version_short = "-v";
-const char *flag_help_short = "-h";
-const char *flag_pump_short = "-p";
-const char *flag_bulk_short = "-b";
-const char *flag_copy_short = "-C";
-const char *flag_length_short = "-l";
-const char *flag_numbers_short = "-n";
-const char *flag_capitalised_short = "-c";
-const char *flag_specialised_short = "-s";
-
-const char *flag_version_long = "--version";
-const char *flag_help_long = "--help";
-const char *flag_pump_long = "--pump";
-const char *flag_bulk_long = "--bulk";
-const char *flag_copy_long = "--copy";
-const char *flag_length_long = "--length";
-const char *flag_numbers_long = "--numbers";
-const char *flag_capitalised_long = "--capitalised";
-const char *flag_specialised_long = "--specialised";
+const char *flag_version = "--version"; // Flag used for version command.
+const char *flag_help = "--help"; // Flag used for help command.
+const char *flag_copy = "--copy";
+const char *flag_length = "--length";
+const char *flag_numbered = "--numbered";
+const char *flag_capitalised = "--capitalised";
+const char *flag_specialised = "--specialised";
 
 const int TRUE = 1;
 const int FALSE = 0;
@@ -34,11 +22,10 @@ const char *numbers = "0123456789";
 const char *specials = "!@#$%^&*-+?";
 
 int length = 12;
-int capitalized = 0;
-int specialized = 0;
-int suffix = 2;
-int bulk = 10;
-int pump = 65536;
+int suffix = 0;
+int capitalised = 0;
+int specialised = 0;
+
 int copied = 0;
 
 char *generate_password() {
@@ -46,27 +33,20 @@ char *generate_password() {
     int sign = TRUE;
 
     for (int i = 0; i < length; i++) {
-        if (sign) {
-            pwd[i] = (char) consonants[arc4random_uniform(strlen(consonants))];
-        } else {
-            pwd[i] = (char) vowels[arc4random_uniform(strlen(vowels))];
-        }
+        if (sign) pwd[i] = (char) consonants[arc4random_uniform(strlen(consonants))];
+        else pwd[i] = (char) vowels[arc4random_uniform(strlen(vowels))];
 
-        sign = (sign) ? FALSE : TRUE;
+        sign = (sign) ? FALSE : TRUE; // Flip sign.
     }
 
     for (int i = 0; i < suffix; i++) {
         pwd[length - 1 - i] = (char) numbers[arc4random_uniform(strlen(numbers))];
     }
 
-    if (capitalized) pwd[0] = (char) toupper(pwd[0]);
-    if (specialized) pwd[length - 1 - suffix] = specials[arc4random_uniform(strlen(specials))];
+    if (capitalised) pwd[0] = (char) toupper(pwd[0]);
+    if (specialised) pwd[length - 1 - suffix] = specials[arc4random_uniform(strlen(specials))];
 
     return pwd;
-}
-
-int check_flag(char *arg, const char *flag_short, const char *flag_long) {
-    return strcmp(arg, flag_short) == 0 || strcmp(arg, flag_long) == 0;
 }
 
 void print_version() {
@@ -76,23 +56,13 @@ void print_version() {
 
 void print_help() {
     printf("Samurai: The Human Readable Password Generator\n\n");
-    printf("Usage: samurai %s | %s | [[%s <int>] [%s <int> | %s ] [%s <int>] [%s <int>] %s %s]\n\n",
-           flag_version_short, flag_help_short, flag_pump_short, flag_bulk_short, flag_copy_short,
-           flag_length_short, flag_numbers_short, flag_capitalised_short, flag_specialised_short);
-    printf("%s, %s\n\tPrint the program's version.\n\n", flag_version_short, flag_version_long);
-    printf("%s, %s\n\tPrint this help message.\n\n", flag_help_short, flag_help_long);
-    printf("%s, %s\n\tSet how many times the random number generator is iterated over before generating each password. Defaults to %d.\n\n",
-           flag_pump_short, flag_pump_long, pump);
-    printf("%s, %s\n\tSet how many passwords are generated. Defaults to %d.\n\n", flag_bulk_short, flag_bulk_long,
-           bulk);
-    printf("%s, %s\n\tOverrides --bulk and instead will generate a single password. This password is copied to the clipboard however instead of printed to the terminal.\n\n",
-           flag_copy_short, flag_copy_long);
-    printf("%s, %s\n\tSet the character length of each password. Defaults to %d.\n\n", flag_length_short,
-           flag_length_long, length);
-    printf("%s, %s\n\tSet the digit length of the numbered suffix, can be zero. Defaults to %d.\n\n",
-           flag_numbers_short, flag_numbers_long, suffix);
-    printf("%s, %s\n\tCapitalise passwords.\n\n", flag_capitalised_short, flag_capitalised_long);
-    printf("%s, %s\n\tUse special characters in the passwords.\n", flag_specialised_short, flag_specialised_long);
+    printf("%s:\n\tPrint the program's version.\n\n", flag_version);
+    printf("%s:\n\tPrint this help message.\n\n", flag_help);
+    printf("%s:\n\tCopy password to clipboard instead of printing.\n\n", flag_copy);
+    printf("%s <int>:\n\tSet the character length of password. Defaults to %d.\n\n", flag_length, length);
+    printf("%s <int>:\n\tSet the digit length of the number suffix. Defaults to %d.\n\n", flag_numbered, suffix);
+    printf("%s:\n\tCapitalise the password. \n\n", flag_capitalised);
+    printf("%s:\n\tEnd password with special character.\n\n", flag_specialised);
     exit(EXIT_SUCCESS);
 }
 
@@ -107,12 +77,11 @@ void print_invalid_integer(char *str) {
 }
 
 void print_invalid_flag(char *flag) {
-    printf("\"%s\" is not a valid flag. Run `samurai %s` or `samurai %s` for usage information.\n", flag,
-           flag_help_short, flag_help_long);
+    printf("\"%s\" is not a valid flag. Run `samurai %s` for usage information.\n", flag, flag_help);
     exit(EXIT_FAILURE);
 }
 
-int read_integer(int* param, const char* arg) {
+int read_integer(int *param, const char *arg) {
     char *ptr;
     *param = (int) strtol(arg, &ptr, 10);
     return ptr == arg;
@@ -120,61 +89,48 @@ int read_integer(int* param, const char* arg) {
 
 void read_params(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
-        if (check_flag(argv[i], flag_capitalised_short, flag_capitalised_long)) {
-            capitalized = TRUE;
-            continue;
-        } else if (check_flag(argv[i], flag_specialised_short, flag_specialised_long)) {
-            specialized = TRUE;
-            continue;
-        } else if (check_flag(argv[i], flag_copy_short, flag_copy_long)) {
+        if (strcmp(argv[i], flag_capitalised) == 0) {
+            capitalised = TRUE;
+        } else if (strcmp(argv[i], flag_specialised) == 0) {
+            specialised = TRUE;
+        } else if (strcmp(argv[i], flag_copy) == 0) {
             copied = TRUE;
-            continue;
-        } else if (check_flag(argv[i], flag_numbers_short, flag_numbers_long)) {
-            if (argc <= i + 1) print_missing_param(argv[i]);
-            i++;
-            if (read_integer(&suffix, argv[i])) print_invalid_integer(argv[i]);
-        } else if (check_flag(argv[i], flag_length_short, flag_length_long)) {
+        } else if (strcmp(argv[i], flag_length) == 0) {
             if (argc <= i + 1) print_missing_param(argv[i]);
             i++;
             if (read_integer(&length, argv[i])) print_invalid_integer(argv[i]);
-        } else if (check_flag(argv[i], flag_bulk_short, flag_bulk_long)) {
+        } else if (strcmp(argv[i], flag_numbered) == 0) {
             if (argc <= i + 1) print_missing_param(argv[i]);
             i++;
-            if (read_integer(&bulk, argv[i])) print_invalid_integer(argv[i]);
-        } else if (check_flag(argv[i], flag_pump_short, flag_pump_long)) {
-            if (argc <= i + 1) print_missing_param(argv[i]);
-            i++;
-            if (read_integer(&pump, argv[i])) print_invalid_integer(argv[i]);
+            if (read_integer(&suffix, argv[i])) print_invalid_integer(argv[i]);
         } else print_invalid_flag(argv[i]);
     }
 }
 
 void execute() {
-    for (int i = 0; i < bulk; i++) {
-        for (int j = 0; j < pump; j++) arc4random();
+    arc4random();
 
-        char *pwd = generate_password();
+    char *pwd = generate_password();
 
-        if (copied) {
-            char cmd[128];
-            strcpy(cmd, "echo ");
-            strcat(cmd, pwd);
-            strcat(cmd, " | pbcopy");
-            system(cmd);
-            exit(EXIT_SUCCESS);
-        }
-
-        printf("%s\n", pwd);
-        free(pwd);
+    if (copied) {
+        char cmd[128];
+        strcpy(cmd, "echo ");
+        strcat(cmd, pwd);
+        strcat(cmd, " | pbcopy");
+        system(cmd);
+        printf("Password has been copied to the clipboard.\n");
+        exit(EXIT_SUCCESS);
     }
+
+    printf("%s\n", pwd);
+    free(pwd);
 }
 
 int main(int argc, char **argv) {
-    if (argc > 1 && check_flag(argv[1], flag_version_short, flag_version_long)) print_version();
-    if (argc > 1 && check_flag(argv[1], flag_help_short, flag_help_long)) print_help();
+    if (argc > 1 && strcmp(argv[1], flag_version) == 0) print_version();
+    if (argc > 1 && strcmp(argv[1], flag_help) == 0) print_help();
 
     read_params(argc, argv);
-
     execute();
 
     exit(EXIT_SUCCESS);
